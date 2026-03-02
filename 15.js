119489d031255e264af4f023fac066939b0cf828
priyabratasahoo780 <priyabrata.sahoo.cg@gmail.com>
2026-03-02 10:48:47 +0530

    2/3/2026

---
+++ 15.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    let a = nums.sort((a,b) => a - b);
+//   for(let i=0; i<nums.length; i++){
+//     for(let j=i+1; j<nums.length; j++){
+//       for(let k=j+1; k<nums.length; k++){
+//         if(i===j || i===k || j===k){
+//           continue;
+//         }
+//         if(nums[i]+nums[j]+nums[k]===0){
+//           return [nums[i],nums[j],nums[k]];
+//         }
+//       }
+//     }
+//   }
+//   return [];
+
+
+ };
+console.log(threeSum([-1,0,1,2,-1,-4]));
---
+++ 34.js
@@ -0,0 +1,16 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    left = 0;
+    right = s.length-1;
+    while(left<right){
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+          left++;
+    right--;
+    }
+};
+console.log(reverseString(["h","e","l","l","o"]))
