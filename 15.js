dab943c75604867a3f13f4b14cd425b41e90e820
priyabratasahoo780 <priyabrata.sahoo.cg@gmail.com>
2026-03-12 20:38:52 +0530

    3sum

--- 15.js
+++ 15.js
@@ -3,21 +3,20 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    let a = nums.sort((a,b) => a - b);
-  for(let i=0; i<nums.length; i++){
-    for(let j=i+1; j<nums.length; j++){
-      for(let k=j+1; k<nums.length; k++){
-        if(i===j || i===k || j===k){
-          continue;
-        }
-        if(nums[i]+nums[j]+nums[k]===0){
-          return [nums[i],nums[j],nums[k]];
-        }
-      }
-    }
-  }
-  return [];
+  nums.sort((a,b) => a-b);
+    let result = [];
 
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            for(let k=j+1;k<nums.length;k++){
+                if(nums[i] + nums[j] + nums[k] === 0){
+                    result.push([nums[i],nums[j],nums[k]]);
+                }
+            }
+        }
+    }
+
+    return result;
 
  };
 console.log(threeSum([-1,0,1,2,-1,-4]));
