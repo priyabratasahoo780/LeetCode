d6f8ca6cc3962d008a915a56393355eb2437f91a
priyabratasahoo780 <priyabrata.sahoo.cg@gmail.com>
2026-04-12 10:45:50 +0530

    practices is practice life time work for you

---
+++ 2778.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfSquares(vector<int>& nums) {
+
+        int n = nums.size();
+        int sumSquare = 0;
+
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                sumSquare += nums[i-1] * nums[i-1]; 
+            }
+        }
+
+        return sumSquare;
+    }
+};
