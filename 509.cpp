291911d2a34a80082c0a5329f02f880268222abb
priyabratasahoo780 <priyabrata.sahoo.cg@gmail.com>
2026-04-19 08:57:45 +0530

    Now to complete a 160 question

---
+++ 509.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+        // if(n == 0) return 0;
+        // if(n == 1) return 1;
+        // return fib(n-1) + fib(n-2);
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        int f = 0;
+        int s = 1;
+        for(int i = 2; i<=n; i++){
+            int current = f + s;
+            f = s;
+            s = current;
+        }
+        return s;
+    }
+};
