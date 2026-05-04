486b6dd1defc5ee557eadc54675ae9669c2b8b6a
priyabratasahoo780 <priyabrata.sahoo.cg@gmail.com>
2026-05-04 21:39:27 +0530

    practice self not cheated

--- 12.cpp
+++ 12.cpp
@@ -13,3 +13,25 @@ public:
         ones[num%10];
     }
 };
+
+// class Solution {
+// public:
+//     string intToRoman(int num) {
+//         vector<int>arr1 = {
+//                         1000 , 900 , 500 , 400 , 100 , 90 , 50 , 40 , 10 , 9 , 5 , 4 , 1
+//     };
+//      vector<string>arr2 = {
+//             "M" , "CM" , "D" , "CD" , "C" , "XC" , "L" , "XL" , "X" , "IX" , "V" , "IV" , "I"  
+//      };
+//     int i = 0;
+//     string r = "";
+//     for(int i=0; i<arr1.size(); i++){
+//         while(num >= arr1[i]){
+//             num -= arr1[i];
+//             r += arr2[i];
+//         }
+        
+//     }
+//      return r;
+//     }
+// };
