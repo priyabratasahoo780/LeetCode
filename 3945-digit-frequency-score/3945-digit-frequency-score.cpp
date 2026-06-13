5bd12274e967c1be6e4a936ced457f91b65a07b7
Priyabrata Sahoo <priyabrata.sahoo.cg@gmail.com>
2026-06-13 10:19:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (65.09%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+          int res = 0;
+        while(n>0)
+        {
+            int rem = n % 10;
+            
+            res += rem;
+            n /= 10;
+        }
+        return res;
+    }
+};
