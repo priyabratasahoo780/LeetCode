84f07ee1460910eed6c584901b8f78d38c92338a
Priyabrata Sahoo <priyabrata.sahoo.cg@gmail.com>
2026-07-22 21:43:13 +0530

    Time: 0 ms (100%), Space: 8.4 MB (98.29%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long result = 0;
+       while(x!=0){
+          result = result*10 + x%10;
+          x = x/10;
+       }
+       if(result > INT_MAX || result < INT_MIN){
+        return 0;
+       }
+       return result; 
+    }
+};
+
