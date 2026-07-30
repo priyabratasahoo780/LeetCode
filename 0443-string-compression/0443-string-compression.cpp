d44706c67fd37fd37bf93f03f4b8a7e73a7bac40
Priyabrata Sahoo <priyabrata.sahoo.cg@gmail.com>
2026-07-30 15:15:24 +0530

    Time: 0 ms (100%), Space: 13.9 MB (48.3%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i = 0;
+        int index = 0;
+        while (i < chars.size()) {
+            char current = chars[i];
+            int count = 0;
+
+            while (i < chars.size() && chars[i] == current) {
+                count++;
+                i++;
+            }
+            chars[index++] = current;
+            if (count > 1) {
+                string s = to_string(count);
+                for (char c : s) {
+                    chars[index++] = c;
+                }
+            }
+        }
+        return index;
+    }
+};
