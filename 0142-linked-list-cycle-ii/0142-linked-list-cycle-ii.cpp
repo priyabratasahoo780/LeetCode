6740fd9f335f5f94c9bb04a2883a952c23adfa37
Priyabrata Sahoo <priyabrata.sahoo.cg@gmail.com>
2026-09-08 11:41:50 +0530

    Time: 8 ms (48.96%), Space: 13.6 MB (6.23%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,22 +9,33 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast != nullptr && fast->next != nullptr){
-            slow = slow->next;
-            fast = fast->next->next;
-             if(fast == slow){
-            slow = head;
+        // ListNode* slow = head;
+        // ListNode* fast = head;
+        // while(fast != nullptr && fast->next != nullptr){
+        //     slow = slow->next;
+        //     fast = fast->next->next;
+        //      if(fast == slow){
+        //     slow = head;
 
-          while(slow!=fast){
-            slow = slow->next;
-            fast = fast->next;
-          }
-          return slow;
+        //   while(slow!=fast){
+        //     slow = slow->next;
+        //     fast = fast->next;
+        //   }
+        //   return slow;
+        // }
+        // }
+        // return nullptr;
+
+        set<ListNode*> address;
+        ListNode* temp = head;
+        while(temp != NULL){
+            if(address.count(temp)){
+                return temp;
+            }
+             address.insert(temp);
+             temp = temp->next;
         }
-        }
-        return nullptr;
+        return NULL;
     }
 };
 
