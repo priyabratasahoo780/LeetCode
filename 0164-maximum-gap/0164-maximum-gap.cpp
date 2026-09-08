6cd12470e4c6f10ef8d4b7c8f6b7c3cd5dce72d7
Priyabrata Sahoo <priyabrata.sahoo.cg@gmail.com>
2026-09-08 11:31:00 +0530

    Time: 47 ms (33.22%), Space: 85.8 MB (91.81%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if(nums.size()<2){
+            return 0;
+        }
+            sort(nums.begin(), nums.end());
+               int maxDiff = 0;
+            for(int i=1; i<nums.size(); i++){
+                     maxDiff = max(maxDiff, (nums[i] - nums[i - 1]));
+                }
+                return maxDiff;
+    }
+};
