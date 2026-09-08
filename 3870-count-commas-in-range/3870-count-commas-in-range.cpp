7799c4a19577b212fdf2452737bc69d72e35d53a
Priyabrata Sahoo <priyabrata.sahoo.cg@gmail.com>
2026-09-08 08:55:09 +0530

    Time: 0 ms (100%), Space: 8.4 MB (84.49%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -3,9 +3,7 @@ public:
     int countCommas(int n) {
         if(n<1000){
             return 0;
-        }else if(n > 999){
-            return n - 1000 + 1;
         }
-          return n;
+          return n - 1000 + 1;
     }
 };
